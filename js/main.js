f124e6f7e4f51ab10b5f51d06e748a1627bb26ec
gogyungho <you@example.com>
2020-05-13 21:36:18 +0900

    aaa

--- js/main.js
+++ js/main.js
@@ -7,18 +7,52 @@
 //     }
 // });
 
+/* 게시 Color Change */
+const commentButton = document.querySelector('.comment_event');
 
-
-document.querySelector('#comment').addEventListener('keydown', function(e){
-    const inputTextValue = document.querySelector('#comment').value;
-    let textStrong = dociment.createElement('strong');
-    let textDiv = document.createElement('div');
-    textStrong.innerHTML = "__goguard";
-    textDiv.appendChild(textStrong);
-    textDiv.appendChild(inputTextValue);
-    if(e.keyCode === 13){
-        
-    document.body.querySelector('.feeds-left').querySelector('.text').appendChild(textDiv);
-     
+commentButton.addEventListener('keyup', function Button(){
+    let Text_aa = document.querySelector('#comment_value').value;
+    if (Text_aa !== ""){
+        return document.querySelector('.comment_upload_button').style.color = '#289aff';
     }
+});
+
+
+
+
+/* 엔티키 이벤트 댓글 */    
+document.querySelector(".comment_event").addEventListener('keydown', function(e){
+        
+        
+    if(e.keyCode === 13){
+        let textDiv = document.createElement('div');
+        let textStrong = document.createElement('strong');
+        textStrong.innerHTML = "__goguard";
+        let inputTextValue = document.querySelector('#comment_value').value;
+        console.log(inputTextValue);
+        let textNode = document.createTextNode(inputTextValue);
+        textDiv.appendChild(textStrong);
+        textDiv.appendChild(textNode);
+        document.body.querySelector('.text-container').appendChild( textDiv );
+        document.querySelector('#comment_value').value=""
+        document.querySelector('#comment_value').blur()
+    }
+});
+
+
+/* 게시 버튼 클릭 댓글 */
+const inputVlaue = document.querySelector(".comment_upload_button");
+        inputVlaue.addEventListener('click', function(){
+        
+            let textDiv = document.createElement('div');
+        let textStrong = document.createElement('strong');
+        textStrong.innerHTML = "__goguard";
+        let inputTextValue = document.querySelector('#comment_value').value;
+        console.log(inputTextValue);
+        let textNode = document.createTextNode(inputTextValue);
+        textDiv.appendChild(textStrong);
+        textDiv.appendChild(textNode);
+        document.body.querySelector('.text-container').appendChild( textDiv );
+        document.querySelector(".comment_upload_button").value ="";
+        document.querySelector("#comment_value").value=""
 });
